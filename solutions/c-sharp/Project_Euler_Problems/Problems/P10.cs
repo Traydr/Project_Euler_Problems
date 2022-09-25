f67927229eb10e812769eb442b9d18920c724a10
Traydr <Traydr@protonmail.com>
2022-09-25 18:17:58 +0300

    P10 Solved!

---
+++ solutions/c-sharp/Project_Euler_Problems/Problems/P10.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Project_Euler_Problems.Problems
+{
+    public class P10
+    {
+        public static void Start()
+        {
+            P10 p10 = new P10();
+            p10.FindSumOfPrimesUnderLimit(2000000);
+        }
+
+        private void FindSumOfPrimesUnderLimit(int limit)
+        {
+            long sumOfPrimes = 0;
+            
+            for (int i = 2; i < limit; i++)
+            {
+                if (Utilities.Primes.IsPrime(i))
+                {
+                    sumOfPrimes += i;
+                }
+            }
+            
+            Console.WriteLine($"The sum of primes is {sumOfPrimes}");
+        }
+    }
+}
